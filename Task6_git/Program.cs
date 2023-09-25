5e7ae4f7e2e2b010980d4f94126a288d5f6cd827
Maxim Saychuk <maks.saychuk@gmail.com>
2023-09-25 14:10:52 +0300

    Feathure add added

--- Task6_git/Program.cs
+++ Task6_git/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,10 +11,36 @@ namespace Task6_git
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите чисо 1")
-                int number 1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Введите число 2")
-                int number 2 = int.Parse(Console.ReadLine());
+        Retry:
+            Console.WriteLine("Введите чисо 1");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите число 2");
+            int number2 = int.Parse(Console.ReadLine());
+        RetryOperand:
+            Console.WriteLine("Ведите функцию +");
+            string operand = Console.ReadLine();
+            int result = 0;
+            switch (operand)
+            {
+                case "+":
+                    {
+                        result = number1 + number2;
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Введена неизвестная функция пожалуйста введите ещё раз");
+                        goto RetryOperand;
+                    }
+
+            }
+            Console.WriteLine($"Результат между {number1}{operand}{number2}={result}");
+            Console.WriteLine("Попробовать ещё раз?\ny/n");
+            string rest = Console.ReadLine();
+            if (rest.ToLower() == "y")
+            {
+                goto Retry;
+            }
         }
     }
 }
